3af903d209478229fedb36ccc241f7c9b6af9a14
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-07-03 21:22:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-FORCE_JOIN = os.environ.get("FORCE_JOIN")
+API_ID = int(os.environ.get("28408963"))
+API_HASH = os.environ.get("b35abdf430d4f8ef6e4311861f6458f8")
+BOT_TOKEN = os.environ.get("8169014867:AAGlZvupoTG702nUQnArBVE7xt5q898mLbI")
+OWNER_ID = int(os.environ.get("7269382715"))
+FORCE_JOIN = os.environ.get("https://t.me/+JUlpxvfITN0zMjVl")
